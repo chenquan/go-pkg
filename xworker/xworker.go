ea1b1f555dc2b3528c419eaefbe1cf396b7e2338
chenquan <chenquan.dev@foxmail.com>
2021-12-06 08:33:10 +0800

    feature(xworker) add Worker (#15)
    * feature(xworker) add Worker
    
    * chore (xworker) remove code

---
+++ xworker/xworker.go
@@ -0,0 +1,44 @@
+/*
+ *    Copyright 2021 chenquan
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package xworker
+
+import (
+	"context"
+	"github.com/chenquan/go-pkg/xtask"
+)
+
+// A Worker is used to run given number of workers to run jobs.
+type Worker struct {
+	c chan struct{}
+}
+
+// NewWorker returns a Worker.
+func NewWorker(size int) *Worker {
+	return &Worker{c: make(chan struct{}, size)}
+}
+
+// Run executes job.
+func (w *Worker) Run(ctx context.Context, run func()) {
+	w.c <- struct{}{}
+	defer func() {
+		<-w.c
+	}()
+	_ = xtask.Do(ctx, func() error {
+		run()
+		return nil
+	})
+}
---
+++ xworker/xworker_test.go
@@ -0,0 +1,55 @@
+/*
+ *    Copyright 2021 chenquan
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package xworker
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+func TestWorker(t *testing.T) {
+	worker := NewWorker(1)
+	group := sync.WaitGroup{}
+	k := 0
+	for i := 0; i < 10; i++ {
+		group.Add(1)
+		worker.Run(context.Background(), func() {
+			group.Done()
+			k++
+		})
+		assert.EqualValues(t, i+1, k)
+	}
+	group.Wait()
+
+	worker = NewWorker(10)
+	group = sync.WaitGroup{}
+	j := uint32(0)
+	for i := 0; i < 100; i++ {
+		group.Add(1)
+		worker.Run(context.Background(), func() {
+			group.Done()
+			atomic.AddUint32(&j, 1)
+		})
+
+	}
+	group.Wait()
+	assert.Equal(t, uint32(100), atomic.LoadUint32(&j))
+
+}
