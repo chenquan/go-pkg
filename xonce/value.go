9d49f7ec72bc5dab5e93cded531d0949b7c00fa6
chenquan <chenquan.dev@foxmail.com>
2022-01-29 20:35:41 +0800

    feature(once): add once module

---
+++ xonce/value.go
@@ -0,0 +1,40 @@
+/*
+ *    Copyright 2021 chenquan
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package xonce
+
+import (
+	"sync/atomic"
+)
+
+type Value struct {
+	v atomic.Value
+}
+
+// NewValue returns a Value.
+func NewValue() *Value {
+	return &Value{}
+}
+
+// Write writes a v.
+func (val *Value) Write(v interface{}) (success bool) {
+	return val.v.CompareAndSwap(nil, v)
+}
+
+// Value return a v.
+func (val *Value) Value() interface{} {
+	return val.v.Load()
+}
---
+++ xonce/value_test.go
@@ -0,0 +1,29 @@
+/*
+ *    Copyright 2021 chenquan
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package xonce
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestOnce(t *testing.T) {
+	value := NewValue()
+	assert.EqualValues(t, true, value.Write(1))
+	assert.EqualValues(t, false, value.Write(2))
+	assert.EqualValues(t, 1, value.Value())
+}
